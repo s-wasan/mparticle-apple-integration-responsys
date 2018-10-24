f60e1767fb4bb3e2518c2707606ad692704427a2
uptscs <uptscs@gmail.com>
2018-10-24 12:06:49 +0530

    copyright to mParticle, Inc.

--- mParticle-Responsys/MPKitResponsys.m
+++ mParticle-Responsys/MPKitResponsys.m
@@ -1,9 +1,21 @@
 //
 //  MPKitResponsys.m
-//  
 //
-//  Created by Upendra Tripathi on 9/17/18.
+//  Copyright 2018 mParticle, Inc.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
 
 #import "MPKitResponsys.h"
 #import "MPEvent.h"
@@ -46,7 +58,7 @@ NSString * const ResponsysEvent = @"ResponsysEvent";
 @implementation MPKitResponsys
 
 +(void) load{
-    MPKitRegister *pioKitRegister = [[MPKitRegister alloc] initWithName:@"Oracle Responsys PushIO" className: @"MPKitResponsys"];
+    MPKitRegister *pioKitRegister = [[MPKitRegister alloc] initWithName:@"Oracle Responsys" className: @"MPKitResponsys"];
     [MParticle registerExtension:pioKitRegister];
 }
 
